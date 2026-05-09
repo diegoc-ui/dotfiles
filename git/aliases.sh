524f276ee08a4001e95b0c7c822ed4a553ff177b
Diego C <248725420+diegoc-ui@users.noreply.github.com>
2026-05-09 13:44:17 -0600

    feat: add git aliases module

---
+++ git/aliases.sh
@@ -0,0 +1,7 @@
+# git aliases I rely on daily
+alias gst='git status -sb'
+alias gco='git checkout'
+alias gsw='git switch'
+alias gcm='git commit -m'
+alias gpl='git pull --ff-only'
+alias gp='git push'
